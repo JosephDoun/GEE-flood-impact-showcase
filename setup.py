ff6ea3efe533fb53903f74de716efdcd1426781a
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-12-10 02:21:37 +0100

    build: setup.py update

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('requirements.txt') as f:
     requirements = f.readlines()
 
 setup(
-    name='Joseph Doundoulakis EO Code Sample',
+    name='Joseph Doundoulakis GEE Flood Impact Showcase',
     version='0.1',
     author='Joseph Doundoulakis',
     email='iosif.doundoulakis@outlook.com',
-    description='EO code sample',
+    description='GEE Flood Impact Showcase',
     install_requires=requirements,
 )
 
